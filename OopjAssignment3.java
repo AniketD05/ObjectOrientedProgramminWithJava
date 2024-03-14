e88cf1de5a1877477e16fcf64e416942c6bd27cc
Aniket Dhiwar <aniketdhiwar1999@gmail.com>
2024-03-14 13:06:17 +0530

    file update

---
+++ OopjAssignment3.java
@@ -0,0 +1,71 @@
+public class OopjAssignment3 {
+    
+    public static void main(String[] args) {
+        /*1) Write a program that demonstrates widening conversion from int to double and 
+        prints the result.*/
+
+       //int num1 = 20;
+       //double num2 = num1;
+       //System.out.println(num2);
+//--------------------------------------------------------------------------------------------------
+       /*2) Create a program that demonstrates narrowing conversion from double to int and 
+       prints the result.*/
+       
+       //double num1 = 500.235d;
+       //int num2 = (int) num1;
+       //System.out.println(num2);
+//--------------------------------------------------------------------------------------------------
+       /*3) Write a program that performs arithmetic operations involving different data types 
+       (int, double, float) and observes how Java handles widening conversions automatically.*/
+
+       /* 
+        int num1 = 10;
+        double num2 = 5.5;
+        float num3 = 2.5f;
+        
+ 
+        double sum1 = num1 + num2;
+        float sum2 = num1 + num3;
+        System.out.println("Addition (int + double): " + sum1);
+        System.out.println("Addition (int + float): " + sum2);
+        
+        double sub1 = num1 - num2;
+        float sub2 = num1 - num3;
+        System.out.println("Subtraction (int - double): " + sub1);
+        System.out.println("Subtraction (int - float): " + sub2);
+        
+        double mult1 = num1 * num2;
+        float mult2 = num1 * num3;
+        System.out.println("Multiplication (int * double): " + mult1);
+        System.out.println("Multiplication (int * float): " + mult2);
+        
+        double div1 = num1 / num2;
+        float div2 = num1 / num3;
+        System.out.println("Division (int / double): " + div1);
+        System.out.println("Division (int / float): " + div2);
+      */
+//--------------------------------------------------------------------------------------------------
+        /*4) Write a Program that demonstrates widening conversion from int to 
+        (double,float, boolean, string) and prints the result. */
+
+        /*
+        int num1 = 10;
+        double num2 = num1;
+        System.out.println(num2);
+
+        int num3 = 20;
+        float num4 =num3;
+        System.out.println(num4);
+
+        int num5 = 30;
+        String num6 = String.valueOf(num5);
+        System.out.println(num6);
+
+        float num7 = 200.23f;
+        String num8 = String.valueOf(num7);
+        System.out.println(num7);
+        */
+
+
+    }
+}
