b12d8e47f8432f77019fa8c28a8c7f4604a44101
Aniket Dhiwar <aniketdhiwar1999@gmail.com>
2024-03-16 15:00:48 +0530

    file update

---
+++ OopjAssignment4Q1.java
@@ -0,0 +1,33 @@
+/* 1) Build a class Student which contains details about the Student 
+      and compile and run its instance. */
+
+public class OopjAssignment4Q1 {
+
+    public static class Student {
+
+        String name;
+        int rollNo;
+        char division;
+
+        public Student(String name, int rollNo, char division) {
+            this.name = name;
+            this.rollNo = rollNo;
+            this.division = division;
+        }
+
+        public void displayStudentDetails() {
+            System.out.println("student Details ---->");
+            System.out.println("Name        : " + this.name);
+            System.out.println("Division    : " + this.division);
+            System.out.println("Roll No     : " + this.rollNo);
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Aniket Dhiwar", 54220064, 'A');
+        student.displayStudentDetails();
+
+    }
+}
